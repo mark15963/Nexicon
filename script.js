48f6f169ecb035601ef19214068fa5888db9ec98
mark15963 <83621809+mark15963@users.noreply.github.com>
2024-12-07 11:50:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function indexData(){
+    var text;
+    document.getElementById("data").innerText = "Главная страница";
+}
+function contactsData(){
+    var text;
+    document.getElementById("data").innerText = "Контакты";
+}
+function aboutData(){
+    var text;
+    document.getElementById("data").innerText = "История компании";
+}
+
