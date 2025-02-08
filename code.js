5781cef623b65a659d24b84d16b5d810c06631c6
mark15963 <83621809+mark15963@users.noreply.github.com>
2025-02-08 16:10:44 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,46 +1,3 @@
-/*
-document.addEventListener("DOMContentLoaded", () => {
-  const apiKey = "86fede440b104e44a0473804250802";
-
-  const searchText = document.getElementById("searchText");
-  const buttonSearch = document.getElementById("buttonSearch");
-  const tableData = document.getElementById("tableData").querySelector("tbody");
-
-  const fetchData = async (cityName) => {
-    try {
-      const response = await fetch(
-        `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no&lang=ru`
-      );
-      const responseJSON = await response.json();
-      return responseJSON;
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
-
-  buttonSearch.addEventListener("click", async () => {
-    const name = searchText.value;
-    if (!name) {
-      alert("no name");
-    } else {
-      const data = await fetchData(name);
-      console.log(data);
-      const tr = document.createElement("tr");
-      const tdName = document.createElement("td");
-      tdName.innerText = data.location.name;
-      tr.appendChild(tdName);
-      const tdTemp = document.createElement("td");
-      tdTemp.innerText = data.current.temp_c;
-      tr.appendChild(tdTemp);
-      const tdWeather = document.createElement("td");
-      tdWeather.innerText = data.current.condition.text;
-      tr.appendChild(tdWeather);
-      tableData.appendChild(tr);
-    }
-  });
-});
-*/
-
 document.addEventListener("DOMContentLoaded", () => {
   const apiKey = "86fede440b104e44a0473804250802";
 
@@ -54,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const fetchData = async (cityName) => {
     try {
       const response = await fetch(
-        `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no&lang=ru`
+        `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=moscow&aqi=no&lang=ru`
       );
       const responseJSON = await response.json();
       return responseJSON;
